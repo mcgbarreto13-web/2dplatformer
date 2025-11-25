0a6b7b335ff3b961ebe4a92f957fc71688601bf2
mcgbarreto13-web <mcgbarreto13@gmail.com>
2025-11-25 18:06:47 -0300

    items and enemy pt2

--- Assets/Scripts/Coin/ItemManager.cs
+++ Assets/Scripts/Coin/ItemManager.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using Core.Singleton;
 
 public class ItemManager : Singleton<ItemManager>
 {
  
     public int coins;
+    public TextMeshProUGUI UITextCoins;
 
    
     private void Start()
@@ -17,10 +19,17 @@ public class ItemManager : Singleton<ItemManager>
     private void Reset()
     {
         coins = 0;
+        UpdateUI();
     }
 
     public void AddCoins(int amount=1)
     {
         coins+= amount;
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        UITextCoins.text = coins.ToString();
     }
 }
